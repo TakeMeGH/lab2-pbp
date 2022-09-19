1ebe7f843fed7eac362aaa3c451c1722987eadf8
Abdul Rafi <abdulrafi907@gmail.com>
2022-09-19 19:02:57 +0700

    fix url mywatchlist html

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -5,7 +5,7 @@ from mywatchlist.views import show_mywatchlist, show_json, show_xml
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
 ]
